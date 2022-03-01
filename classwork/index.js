d4d215d00120f2ef855867b6b615105444edf71f
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2022-03-01 12:18:15 -0600

    JS Day 2 - classwork

---
+++ classwork/index.js
@@ -0,0 +1,63 @@
+// Scoped demonstration
+// let a = 5, b = 10;
+
+// if (a > b) {
+//     let c = 50; // Scope of c is only within if block
+// } else {
+//     let c = 100;
+// }
+
+// console.log(c);
+
+
+// // Declaration
+// // let
+// let user = 'John';
+// console.log(user);
+// user = 'Mike';
+
+// console.log(user);
+
+// // var
+// var user = 'John';
+// var user = 'Mike';
+
+// console.log(user);
+
+
+// // Convention
+// let userProfile; // camelCase
+
+
+// // const
+// const COLOR = 'red';
+
+// console.log(COLOR);
+
+
+// console.log('Test'/0); // NaN
+
+
+// Nested Expression
+// let a = 10, b = 20;
+
+// console.log(`Sum of a and b is: ${ a + b }`); // Expression(inside '{}') is always followed by $
+
+
+// let age = "Test";
+
+// console.log(typeof(age));
+
+
+// '==' vs. '==='
+// console.log(5 == '5'); // true - 5(number) loosely equals 5(string)
+// console.log(5 === '5'); // false - strict equal(===) forces typeof to match as well as the data itself
+
+
+// // Increment
+// let a = 10;
+// console.log(a);
+// console.log(++a); // a++ (increments pre-display)
+// console.log(a++); // a++ (increments post-display)
+// a++;
+// console.log(a);
