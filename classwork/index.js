6419417d5afcba7fcd78ca325094d7ca2cd2f81b
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2022-03-03 12:05:41 -0600

    JS Day 4 - classwork

--- classwork/index.js
+++ classwork/index.js
@@ -98,13 +98,141 @@
 // };
 
 
-// String manipulation - targeting partial strings(substrings)
-let str = 'Hi how are you?';
+// // String manipulation - targeting partial strings(substrings)
+// let str = 'Hi how are you?';
 
-str[0] = 'h';
+// str[0] = 'h';
 
-console.log(str[0]);
+// console.log(str[0]);
 
-console.log(str.substring(3,10));
+// console.log(str.substring(3,10));
 
-console.log(str.substr(3,10)); // 'substr' - depricated
+// console.log(str.substr(3,10)); // 'substr' - depricated
+
+
+// !!! ARRAYS !!!
+
+let names = ['John', 'Mike', 'Peter', 'Kevin'];
+
+// console.log(names[1]);
+
+// console.log('Array Length ' + names.length);
+
+// // for ('each index')
+// for (let i = 0; i < names.length; i++) {
+//     // if the first letter is 'P'
+//     if (names[i].startsWith('P')) {
+//         // continue skips the rest of the logic in the for loop, in this case, if the if statement condition( (names[i].startsWith('P')) ) is met
+//         continue;
+//     };
+
+//     // clog the indexes that make it to this point in the function
+//     console.log(names[i]);
+// };
+
+// // '.pop()' - removes the highest index
+// console.log(names);
+// console.log(names.pop());
+// console.log(names);
+
+// // .push - appends another index to the array
+// console.log(names);
+// names.push('Sara');
+// console.log(names.push('Sara'));
+// console.log(names);
+
+// // '.shift()' - removes index 0
+// // '.unshift()' - inserts an new value as index 0, shifting/incrementing all previous indexs by 1
+// console.log(names);
+// names.shift();
+// console.log(names);
+// names.unshift('Sara');
+// console.log(names);
+
+// // '.slice()' - Returns a copy of the array at only the indexs specified in the params
+// console.log(names);
+// names.slice(0,3);
+// console.log(names.slice(1,3));
+// console.log(names);
+
+// for (let name of names) {
+//     console.log(name);
+// };
+
+// names[36] = 'Test';
+// console.log(names);
+// console.log(names.length);
+
+
+// // !!! FUNCTION DECLARATION !!! 
+// function hello() {
+//     console.log('hello');
+// };
+
+// // !!! FUNCTION EXPRESSION !!!
+// let hello = function() {
+//     console.log('Hello');
+// };
+
+// console.log(typeof hello);
+// hello();
+
+// !!! CALLBACK FUNCTIONS !!!
+
+// // // !!! ARROW FUNCTION !!!
+// // function declaration
+// function test0(a, b) {
+//     let c = a + b;
+//     return c;
+// };
+// console.log(test0(1, 1));
+// console.log(typeof test0);
+
+// // function expression
+// let test = function(a, b) {
+//     let c = a + b;
+//     return c;
+// };
+// console.log(test(10, 10));
+// console.log(typeof test);
+
+// // arrow function
+// let test1 = (a, b) => {
+//     let c = a + b;
+//     return c;
+// };
+// console.log(test1(20, 20));
+// console.log(typeof test1);
+
+// // the three above function output the same results
+
+
+// declare numbers varriable equal to an array
+let numbers = [12, 23, 18, 26, 40];
+// '.filter()' - creates a new array with all elements that pass the test implemented by the provided function
+let evenNumbers = numbers.filter(getEven);
+
+function getEven(num) {
+    return num%2 == 0;
+};
+
+console.log('Filtered: ' + evenNumbers);
+console.log('Original: ' + numbers);
+
+// '.map()' - creates a new array populated with the results of calling a provided function on every element in the calling array
+let squaredNum = numbers.map(getSquared);
+
+function getSquared(num) {
+    return num * num;
+};
+
+console.log('Squared: ' + squaredNum);
+
+// '.reduce()' - executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value
+let sum = numbers.reduce(getSum, 0);
+
+function getSum(total, sum) {
+    return total + sum;
+};
+
+console.log('Sum of all numbers: ' + sum);
